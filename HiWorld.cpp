6adbfcc3ce53b390e01f93799522f874237dd31b
jtang3 <tang@mpp.mpg.de>
2014-04-08 11:13:40 +0200

    first time

---
+++ HiWorld.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : HiWorld.cpp
+// Author      : Jian Tang
+// Version     :
+// Copyright   : GPL
+// Description : Hi World in C++
+//============================================================================
+
+#include <iostream>
+#define EXIT_SUCCESS 0
+
+using namespace std;
+
+int main(void) {
+	cout << "Hi World!";
+	return EXIT_SUCCESS;
+}
